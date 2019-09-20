c2fb43d39d9d3ba705055d9f8adc3492d81946c4
AlexHuts <vatslav.zoltan@gmail.com>
2019-09-20 16:33:31 +0300

    100%

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -5,4 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
       enabled: true
     }
   });
+  $('a[href*="#"]').on('click', function (e) {
+		e.preventDefault();
+		$('.page_nav').removeClass('active');
+		$('.hamburger').removeClass('active');
+	
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top-100
+		}, 500, 'linear');
+  });
+  $('.hamburger').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('.page_nav').toggleClass('active');
+	});
 });
