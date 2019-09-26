4a12f58d7edfacdb7a9d5fd6bfc3a42cb22d8d94
vatslav.zoltan@gmail.com <vatslav.zoltan@gmail.com>
2019-09-26 22:27:43 +0300

    form updates

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -5,18 +5,58 @@ document.addEventListener("DOMContentLoaded", function() {
       enabled: true
     }
   });
-  $('a[href*="#"]').on('click', function (e) {
-		e.preventDefault();
-		$('.page_nav').removeClass('active');
-		$('.hamburger').removeClass('active');
-	
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top-100
-		}, 500, 'linear');
-  });
-  $('.hamburger').on('click', function(e){
+  $('a[href*="#"]').on("click", function(e) {
     e.preventDefault();
-    $(this).toggleClass('active');
-    $('.page_nav').toggleClass('active');
-	});
+    $(".page_nav").removeClass("active");
+    $(".hamburger").removeClass("active");
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100
+      },
+      500,
+      "linear"
+    );
+  });
+  $(".hamburger").on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".page_nav").toggleClass("active");
+  });
+});
+$(document).ready(function() {
+  //E-mail Ajax Send
+  $("form.callback_banner, form.callback_page").submit(function(e) {
+    //Change
+    e.preventDefault();
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "telegram.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      $.ajax({
+        type: "POST",
+        url: "mail.php", //Change
+        data: th.serialize()
+      }).done(function() {
+        console.log("Sent to mail");
+      });
+      $(th)
+        .find(".success")
+        .addClass("active")
+        .css("display", "flex")
+        .hide()
+        .fadeIn();
+      setTimeout(function() {
+        $(th)
+          .find(".success")
+          .removeClass("active")
+          .fadeOut();
+        th.trigger("reset");
+      }, 3000);
+    });
+
+    return false;
+  });
 });
